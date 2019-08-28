5c5553d81dabc4d794ec3b5771069903816cb327
HexChrx <chrx0621@gmail.com>
2019-08-28 14:30:09 +0800

    Update router.go

--- routers/router.go
+++ routers/router.go
@@ -2,8 +2,8 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/chrx/testgokafka/middlewares"
-	"github.com/chrx/testgokafka/controllers"
+	"github.com/HexChrx/testgokafka/middlewares"
+	"github.com/HexChrx/testgokafka/controllers"
 )
 
 // Init  web路由初始化
